e4d6b88ae913c9137da3e0a3145c732031908d2c
Morgante Pell <morgante.pell@morgante.net>
2016-09-12 12:16:06 -0400

    Add resume

--- config.rb
+++ config.rb
@@ -45,8 +45,9 @@ redirect "facebook.html", to: "/internships/facebook"
 redirect ".well-known/keybase.txt.html", to: "https://gist.githubusercontent.com/morgante/9899709/raw/627116ae34ce28a07f4ee77eb8550782c8fb48db/gistfile1.txt"
 redirect "drf.html", to: "https://drive.google.com/file/d/0B0RTahF1SjYvdXROeWMtY0tEeFE/edit?usp=sharing"
 redirect "valentine/2013.html", to: "http://fierce-eyrie-7449.herokuapp.com"
-redirect "resume.html", to: "http://me.morgante.net/resume.pdf"
-redirect "calendar.html", to: "http://me.morgante.net/schedule"
+redirect "resume.html", to: "http://morgante.net/resume.pdf"
+redirect "calendar.html", to: "http://morgante.net/schedule"
+redirect "user/files/vcard.vcf.html", to: "/files/vcard.vcf"
 
 # Build-specific configuration
 configure :build do
