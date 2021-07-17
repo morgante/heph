941f41c7013d03927ccd52d640f4b802f4ade2a6
Morgante Pell <morgantep@google.com>
2021-07-17 02:27:33 -0400

    Make typescript work

---
+++ heph/src/pages/hello.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { PageProps, Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+type DataProps = {
+  site: {
+    buildTime: string
+  }
+}
+
+const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
+  <Layout>
+    <Seo title="I love typescript" />
+    <h1>This is typescript.</h1>
+  </Layout>
+)
+
+export default UsingTypescript
+
+export const query = graphql`
+  {
+    site {
+      buildTime(formatString: "YYYY-MM-DD hh:mm a z")
+    }
+  }
+`
