753afa467012434379f0aa574cb2313cc1544c91
Morgante Pell <morgantep@google.com>
2021-07-19 17:01:31 -0400

    Try adding Material UI

--- heph/src/pages/github.tsx
+++ heph/src/pages/github.tsx
@@ -1,9 +1,12 @@
 import * as React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
+import { Button } from '@material-ui/core';
+
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import GitHubActivity from "../components/github_activity"
+
 
 type DataProps = {
   site: {
@@ -14,6 +17,7 @@ type DataProps = {
 const Page: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <Seo title="I love typescript" />
+    <Button variant="contained" color="primary" disabled>Hello</Button>
     <GitHubActivity
       username="morgante"
       message="I love GitHub lots." />
